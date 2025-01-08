86f85d8ee1466abac8737d2b334ee8c65359d6e9
alex-ande <alexandertigre328@gmail.com>
2025-01-08 12:46:43 -0800

    arboles

---
+++ src/App.java
@@ -0,0 +1,17 @@
+import Main.Materia.Controllers.ArbolBinario;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        runArbolBinario();
+    }
+
+    public static void runArbolBinario() {
+        ArbolBinario arbolBinario = new ArbolBinario();
+        int[] valores = { 40, 20, 60, 10, 30, 50, 70, 5, 15, 55 };
+        
+        for (int value : valores) {
+            arbolBinario.insert(value);
+            arbolBinario.printTree();
+        }
+    }
+}
